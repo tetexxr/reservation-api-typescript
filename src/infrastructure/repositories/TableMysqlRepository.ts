2f9063f9971efda17d23d86cdb4bad994367e6fe
Xavier Xiqués <xavier.xiques@gmail.com>
2025-05-31 17:31:36 +0200

    Implementation of Mysql repository for tables + seed data

---
+++ src/infrastructure/repositories/TableMysqlRepository.ts
@@ -0,0 +1,13 @@
+import { injectable } from 'tsyringe'
+import { db } from '../database/config'
+import { Table } from '@/domain/tables/Table'
+import { TableNumber } from '@/domain/tables/TableNumber'
+import { TableRepository } from '@/domain/tables/TableRepository'
+
+@injectable()
+export class TableMysqlRepository implements TableRepository {
+  async findAll(): Promise<Table[]> {
+    const tables = await db.selectFrom('tables').selectAll().execute()
+    return tables.map((table) => new Table(new TableNumber(table.table_number), table.maximum_seating_capacity))
+  }
+}
---
+++ tests/integration/helpers/seeds/tableSeeds.ts
@@ -0,0 +1,27 @@
+import { Table } from '@/domain/tables/Table'
+import { TableNumber } from '@/domain/tables/TableNumber'
+import { db } from '@/infrastructure/database/config'
+
+const tables = [
+  new Table(new TableNumber(1), 2),
+  new Table(new TableNumber(2), 2),
+  new Table(new TableNumber(3), 4),
+  new Table(new TableNumber(4), 4),
+  new Table(new TableNumber(5), 6),
+  new Table(new TableNumber(6), 6),
+  new Table(new TableNumber(7), 8),
+  new Table(new TableNumber(8), 10)
+]
+
+export async function seedTables(): Promise<void> {
+  await db.deleteFrom('tables').execute()
+  for (const table of tables) {
+    await db
+      .insertInto('tables')
+      .values({
+        table_number: table.tableNumber.value,
+        maximum_seating_capacity: table.maximumSeatingCapacity
+      })
+      .execute()
+  }
+}
