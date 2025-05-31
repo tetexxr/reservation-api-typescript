5696a7b5631fb6ab208668a6583fdb09563a4f0a
Xavier Xiqués <xavier.xiques@gmail.com>
2025-05-31 17:31:56 +0200

    Database migration for create tables

---
+++ src/infrastructure/database/migrations/20240320143000_create_tables_table.ts
@@ -0,0 +1,14 @@
+import { Kysely } from 'kysely'
+import { Database } from '../config'
+
+export async function up(db: Kysely<Database>): Promise<void> {
+  await db.schema
+    .createTable('tables')
+    .addColumn('table_number', 'integer', (col) => col.primaryKey())
+    .addColumn('maximum_seating_capacity', 'integer', (col) => col.notNull())
+    .execute()
+}
+
+export async function down(db: Kysely<Database>): Promise<void> {
+  await db.schema.dropTable('tables').execute()
+}
