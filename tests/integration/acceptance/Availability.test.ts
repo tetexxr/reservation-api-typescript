edb0e575410955baf980aa13b3da1a396cddc878
Xavier Xiqués <xavier.xiques@gmail.com>
2025-05-31 11:52:31 +0200

    Availability acceptance tests

---
+++ tests/integration/acceptance/Availability.test.ts
@@ -0,0 +1,109 @@
+import 'reflect-metadata'
+import { describe, it, beforeEach, expect } from 'vitest'
+import app from '@/app'
+import { container } from 'tsyringe'
+import { CreateReservation } from '@/application/reservations/CreateReservation'
+import { CreateReservationCommand } from '@/application/reservations/CreateReservationCommand'
+import { CustomerDetails } from '@/domain/reservations/CustomerDetails'
+import { Reservation } from '@/domain/reservations/Reservation'
+import { Cleaner } from '../helpers/Cleaner'
+
+describe('Availability', () => {
+  const createReservation = container.resolve(CreateReservation)
+  const cleaner = container.resolve(Cleaner)
+
+  beforeEach(async () => {
+    await cleaner.execute()
+  })
+
+  it('should get all available slots', async () => {
+    const times = [
+      '2021-10-10T08:00:00',
+      '2021-10-10T08:30:00',
+      '2021-10-10T09:00:00',
+      '2021-10-10T09:25:00',
+      '2021-10-10T09:50:00',
+      '2021-10-10T10:25:00',
+      '2021-10-10T10:45:00',
+      '2021-10-10T11:15:00',
+      '2021-10-10T11:55:00',
+      '2021-10-10T12:10:00',
+      '2021-10-10T12:50:00',
+      '2021-10-10T13:15:00'
+    ]
+    for (const t of times) {
+      await createReservation.execute(new CreateReservationCommand(reservationAt(new Date(t))))
+    }
+
+    const response = await app.inject({
+      method: 'GET',
+      url: '/v1/availability',
+      query: { date: '2021-10-10', partySize: '4' }
+    })
+
+    expect(JSON.parse(response.body)).toEqual({
+      items: [
+        {
+          from: '2021-10-10T08:00:00',
+          to: '2021-10-10T08:15:00',
+          tableNumber: 4
+        },
+        {
+          from: '2021-10-10T08:15:00',
+          to: '2021-10-10T08:30:00',
+          tableNumber: 4
+        },
+        {
+          from: '2021-10-10T08:45:00',
+          to: '2021-10-10T09:00:00',
+          tableNumber: 3
+        },
+        {
+          from: '2021-10-10T10:10:00',
+          to: '2021-10-10T10:25:00',
+          tableNumber: 4
+        },
+        {
+          from: '2021-10-10T11:30:00',
+          to: '2021-10-10T11:45:00',
+          tableNumber: 3
+        },
+        {
+          from: '2021-10-10T12:55:00',
+          to: '2021-10-10T13:10:00',
+          tableNumber: 4
+        },
+        {
+          from: '2021-10-10T13:35:00',
+          to: '2021-10-10T13:50:00',
+          tableNumber: 3
+        }
+      ],
+      total: 7
+    })
+  })
+
+  it('should get empty when no available slots', async () => {
+    const opening = new Date('2021-10-10T08:00:00')
+    for (let i = 0; i <= 23; i++) {
+      const time = new Date(opening.getTime() + 45 * 60_000 * i)
+      await createReservation.execute(new CreateReservationCommand(reservationAt(time)))
+      await createReservation.execute(new CreateReservationCommand(reservationAt(time)))
+    }
+
+    const response = await app.inject({
+      method: 'GET',
+      url: '/v1/availability',
+      query: { date: '2021-10-10', partySize: '4' }
+    })
+
+    expect(JSON.parse(response.body)).toEqual({
+      items: [],
+      total: 0
+    })
+  })
+})
+
+function reservationAt(time: Date): Reservation {
+  return Reservation.create(time, new CustomerDetails('John', 'john@test.com', '931111111'), 4)
+}
